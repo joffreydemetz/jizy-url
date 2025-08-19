d3554d730129b8d77434a75a1ddbf955f063d44e
Joffrey Demetz <joffrey.demetz@gmail.com>
2025-08-19 11:54:44 +0200

    Use jizy-packer

--- config/jpack.js
+++ config/jpack.js
@@ -1,31 +1,22 @@
-import fs from 'fs';
-import path from 'path';
-import {
-    jPackConfig,
-    LogMe
-} from "../jpack/utils.js";
-
-const currentPath = path.dirname(import.meta.url);
-
-jPackConfig.init({
+const jPackData = {
     name: 'jUrl',
     alias: 'jizy-url',
     cfg: 'url',
     assetsPath: 'dist',
-    checkConfig: (config) => {
-        return config;
-    },
-    genBuildJs: (code, config) => {
-        return code;
-    },
-    onPacked: (config) => {
-        const target = jPackConfig.get('assetsPath');
 
-        // move the .min.js file in dist/js/ to dist/
-        LogMe.log('Move minified JS file to dist root');
-        const minJsFile = path.join(target, 'js', `${jPackConfig.get('alias')}.min.js`);
-        if (fs.existsSync(minJsFile)) {
-            fs.renameSync(minJsFile, path.join(target, `${jPackConfig.get('alias')}.min.js`));
-        }
-    }
-});
+    buildTarget: null,
+    buildZip: false,
+    buildName: 'default',
+
+    desktopBreakpoint: '900px',
+
+    onCheckConfig: () => { },
+
+    onGenerateBuildJs: (code) => code,
+
+    onGenerateWrappedJs: (wrapped) => wrapped,
+
+    onPacked: () => { }
+};
+
+export default jPackData;
